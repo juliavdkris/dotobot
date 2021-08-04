2c99de2c80fed28263880d15ffd063fae72a74a4
maurits <mauritskienhuis@gmailcom>
2021-08-04 22:52:02 +0200

    Implement !speeddate

--- src/packages/misc.py
+++ src/packages/misc.py
@@ -1,8 +1,11 @@
+import asyncio
 import logging
 from os.path import basename
 from random import choice
+from typing import List
 
 import discord
+from discord.colour import Colour
 from discord.ext import commands
 
 # -------------------------> Main
@@ -16,6 +19,70 @@ def setup(bot: commands.Bot) -> None:
 def teardown(bot: commands.Bot) -> None:
 	log.info(f'Module has been de-activated: {basename(__file__)}')
 
+async def delete_category(category: discord.CategoryChannel) -> None:
+	for channel in category.channels:
+		try:
+			await channel.delete()
+		except:
+			log.warning(f'Failed to delete a speeddating VC in {category.guild.name}')
+	await category.delete()
+
+def is_member_in_vc(user: discord.Member) -> bool:
+	try:
+		user.voice
+		return True
+	except:
+		return False
+
+async def move_all(users: List[discord.Member], voice_channel: discord.VoiceChannel) -> None:
+	for user in users:
+		try:
+			await user.move_to(voice_channel)
+		except:
+			pass
+
+def parse_validVC_mentions_from_ctx(ctx: commands.Context) -> List[discord.Member]:
+	users = [role.members for role in ctx.message.role_mentions]
+	users.append(ctx.message.mentions)
+	users += [[ctx.author]]
+	users = list(set(sum(users, [])))
+	if len(users) == 1:
+		try:
+			users = ctx.author.voice.channel.members
+		except:
+			pass
+	users = [user for user in users if is_member_in_vc(user)]
+	return users
+
+# https://stackoverflow.com/questions/6648512/scheduling-algorithm-for-a-round-robin-tournament
+def roundrobin(users: List) -> List:
+	if len(users) % 2 == 1:
+		users.append(None)
+	rounds = []
+	one = users.pop(0)
+	for _ in range(len(users)):
+		top_row = [one] + users[:len(users)//2]
+		bot_row = users[::-1][:1 + len(users)//2]
+		rounds.append([(top_row[i], bot_row[i]) for i in range(len(bot_row))])
+		users.insert(0,users.pop())
+	return rounds
+
+async def speed_date_roullete(rounds: List, channel: discord.TextChannel, category: discord.CategoryChannel, users: List, lounge: discord.VoiceChannel) -> None:
+	for turn in rounds:
+		currentVC = 1  # lounge is at 0
+		for pair in turn:
+			if None in pair:
+				continue
+			for user in pair:
+				try:
+					await user.move_to(category.voice_channels[currentVC])
+				except:
+					await channel.send(embed=discord.Embed(title='⛔ Failed to move {user.name} into their speed-date', colour=Colour.from_rgb(0,0,0)))
+			currentVC += 1
+
+		await asyncio.sleep(5)
+		await move_all(users, lounge)
+		await asyncio.sleep(5)
 
 class Miscellaneous(commands.Cog, name='Misc', description='Novelty functionality'):
 	def __init__(self, bot: commands.Bot):
@@ -81,3 +148,35 @@ class Miscellaneous(commands.Cog, name='Misc', description='Novelty functionalit
 			await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=msg[9:]))
 		else:
 			await self.bot.change_presence(activity=discord.Game(name=msg))
+
+	@commands.command(brief='Call for a speeddating session', description='Call for a speeddating session by tagging members who are in a VC', usage='[@friend, @role] / current vc')
+	@commands.has_permissions(administrator=True)
+	async def speeddate(self, ctx: commands.Context):
+		users = parse_validVC_mentions_from_ctx(ctx)
+		if len(users) == 1:
+			await ctx.send(embed=discord.Embed(title='⛔ Not enough valid users', colour=Colour.from_rgb(0,0,0), description='Make sure you are either in a voice channel with more than 1 person.\nOr make sure to ping enough people who are currently in a voice channel.'))
+		original_voice_state = {user.id: user.voice.channel for user in users}
+
+		# setting up the necessary channels
+		category = await ctx.guild.create_category     (name='Speed-dating', overwrites={ctx.guild.default_role: discord.PermissionOverwrite(connect = False)})
+		Tchannel = await category. create_text_channel (name='Speed-dating')
+		VClounge = await category. create_voice_channel(name='The-Lounge')
+		for i in range(len(users)//2):
+			await category.create_voice_channel(name=f'Date: {i+1}')
+
+		# trigger speed-dating scheduler
+		rounds = roundrobin(list(users))
+		print(rounds)
+		print(users)
+		await speed_date_roullete(rounds, Tchannel, category, users, VClounge)
+
+		# move all users back to their original voice channels and clean-up
+		for user in users:
+			try:
+				await user.move_to(original_voice_state[user.id])
+			except AttributeError:
+				pass
+
+		await Tchannel.send(f'This channel and all accompanying channels will be deleted in 5 seconds')  # TODO bigger number here
+		await asyncio.sleep(5)
+		await delete_category(category)
