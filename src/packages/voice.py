42616e7afe34505e38ed836299e15fb06d994fbb
Maurits Kienhuis <mauritskienhuis@gmail.com>
2022-04-09 22:21:07 +0200

    try to avoid immediate reconnects when possible
    so scuffed

--- src/packages/voice.py
+++ src/packages/voice.py
@@ -34,19 +34,22 @@ class Voice(commands.Cog, description='Play music in voice'):
 	# Plays a sound in voice
 	async def voice_helper(self, vc, file):
 		try:
-			vc = await vc.connect()
+			client = await vc.connect()
 		except:
-			await [client for client in self.bot.voice_clients if client.guild == vc.guild][0].disconnect()
-			vc = await vc.connect()
+			if len([client for client in self.bot.voice_clients if client.channel == vc]) != 0:  # very problematic as this could take over a client just when it is about to disconnect, should be synced in reality
+				client = [client for client in self.bot.voice_clients if client.channel == vc][0]
+			else:
+				await [client for client in self.bot.voice_clients if client.guild == vc.guild][0].disconnect()
+				client = await vc.connect()
 
-		vc.play(discord.FFmpegPCMAudio(source='storage/static/sounds/' + file))
-		while vc.is_playing():
+		client.play(discord.FFmpegPCMAudio(source='storage/static/sounds/' + file))
+		while client.is_playing():
 			await sleep(2)
-		await vc.disconnect()
+		await client.disconnect()
 
 	# Plays crabrave in voice
 	async def crabrave(self, text_channel, vc, arg=None):
-		path = 'crab_rave.mp3'
+		path = 'crab_rave.mp3' if randint(0, 20) != 0 else 'under_rave.mp3'
 		await text_channel.send(f"{arg} IS GONE :crab: :crab: :crab: :crab: :crab: :crab: :crab:")  # crab rave gif / shortened version of crab rave
 		await text_channel.send('https://tenor.com/view/crab-safe-dance-gif-13211112')
 		await self.voice_helper(vc, path)
@@ -64,7 +67,7 @@ class Voice(commands.Cog, description='Play music in voice'):
 		elif 'play zoutelande' in content or 'speel zoutelande' in content:
 			await self.voice_helper(msg.author.voice.channel, 'zoutelande.mp3')
 		elif 'tequila' in content:
-			await self.voice_helper(msg.author.voice.channel, 'hola_cabron.mp3')
+			await self.voice_helper(msg.author.voice.channel, 'tequila.mp3')
 
 	# Plays crabrave in voice
 	@commands.command(brief='Stuff is gone', description='Stuff is gone.', usage='my social life')
